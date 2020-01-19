690c522f23b8edea3f2f5d206db45686944d7c50
Fúlvio B. Nedel <fulvionedel@gmail.com>
2020-01-19 12:41:39 -0300

    Leitura dos dados
    Mudei a ordem de leitura, agora inicio lendo o arquivo no computador.
    Coloquei comentários para informar sobre o processo.

--- data-raw/aih100.R
+++ data-raw/aih100.R
@@ -23,14 +23,20 @@
 #' @keywords datasets
 "aih100"
 
+# Ler o "arquivo da AIH"
+## Sem acesso ao ftp do DATASUS
+aih <- read.dbc::read.dbc("../RDRS1801.dbc")
+## Com acesso ao ftp do DATASUS 
 temp <- tempfile()
 download.file(url="ftp://ftp.datasus.gov.br/dissemin/publicos/SIHSUS/200801_/Dados/RDRS1801.dbc", destfile = temp)
 aih <- read.dbc::read.dbc(temp)
 
-aih <- read.dbc::read.dbc("../RDRS1801.dbc")
+# Selecionar uma amostra aleatória de 100 registros e todas as variáveis da AIH
 aih100 <- aih[sample(rownames(aih), 100), ]
 rownames(aih100) <- NULL
 str(aih100)
+
+# Criar o arquivo dados com os registros selecionados
 write.csv2(aih100, file = "data-raw/aih100.csv")
 # save(aih100, file = "data/aih100.rda")
 usethis::use_data(aih100, overwrite = TRUE, compress = "xz")
