92ebb9c8732983766887789357808f1592f6bcae
Fúlvio B. Nedel <fulvionedel@gmail.com>
2023-04-13 21:20:12 -0300

    Arrumada definição do fator em 'fxetaria'

--- R/ler_popbr.R
+++ R/ler_popbr.R
@@ -11,11 +11,11 @@
 #' popBR2012 <- ler_popbr("POPBR12.DBF")
 #' head(popBR2012)
 #' xtabs(populacao ~ fxetar5, data = popBR2012)
+#' }
 #'
 #' # Um arquivo no diretório FTP do DATASUS
 #' popBR2012 <- ler_popbr("popBR12", source = "url")
 #' xtabs(populacao ~ fxetar5, data = popBR2012)
-#' }
 #'
 #' @importFrom utils download.file unzip
 #' @importFrom Hmisc upData
@@ -29,31 +29,32 @@ ler_popbr <- function (pop, source = "file")
       temp <- tempfile()
       download.file(url, temp)
       unzip(temp, paste0(pop, ".DBF"))
-      pop <- foreign::read.dbf(paste0(pop, ".DBF"))
+      populacao <- foreign::read.dbf(paste0(pop, ".DBF"))
       unlink(temp)
+      unlink(paste0(pop, ".DBF"))
     } else
-      if(source == "file") pop <- foreign::read.dbf(pop)
+      if(source == "file") populacao <- foreign::read.dbf(pop)
 
-   pop <- Hmisc::upData(pop, lowernames = T, print = F)
-   pop$fxetar5 <- cut(as.numeric(pop$fxetaria),
-                       breaks = c(0, 5, 10, 15, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33))
-    levels(pop$fxetar5) <- c("0-4", "5-9", "10-14", "15-19",
-                             "20-24", "25-29", "30-34", "35-39", "40-44", "45-49",
-                             "50-54", "55-59", "60-64", "65-69", "70-74", "75-79",
-                             "80 +")
-    pop$fxetaria <- factor(pop$fxetaria, levels = 1:33, labels = c("< 1 ano", 1:19, levels(pop$fxetar5)[5:17]))
-    levels(pop$sexo) <- c("masc", "fem")
-    if ( length( levels(pop$situacao)>1 ) ) {
-      levels(pop$situacao) <- c("urbana", "rural")
+   populacao <- Hmisc::upData(populacao, lowernames = T, print = F)
+   populacao$fxetar5 <- cut(as.numeric(populacao$fxetaria),
+                            breaks = c(0, 5, 10, 15, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33))
+   levels(populacao$fxetar5) <- c("0-4", "5-9", "10-14", "15-19",
+                                  "20-24", "25-29", "30-34", "35-39", "40-44", "45-49",
+                                  "50-54", "55-59", "60-64", "65-69", "70-74", "75-79",
+                                  "80 +")
+   levels(populacao$fxetaria) <- c("< 1 ano", 1:19, levels(populacao$fxetar5)[5:17])
+   levels(populacao$sexo) <- c("masc", "fem")
+   if ( length( levels(populacao$situacao)>1 ) ) {
+      levels(populacao$situacao) <- c("urbana", "rural")
     }
-    pop$ano <- as.integer(as.character(pop$ano))
-    attr(pop$fxetar5, which = "label") <- "Faixa etaria quinquenal"
-    attr(pop$fxetaria, which = "label") <- "Faixa etaria detalhada"
-    attr(pop$munic_res, which = "label") <- "Codigo IBGE do municipio"
-    # Hmisc::label(pop$fxetar5) <- "Faixa etaria quinquenal"
-    # names(pop)[5] <- "fxetar.det"
-    # Hmisc::label(pop$fxetar.det) <- "Faixa etaria detalhada"
-    # Hmisc::label(pop$fxetaria) <- "Faixa etaria detalhada"
-    # Hmisc::label(pop$munic_res) <- "Codigo IBGE do municipio"
-    return(pop)
+   populacao$ano <- as.integer(as.character(populacao$ano))
+   attr(populacao$fxetar5, which = "label") <- "Faixa etaria quinquenal"
+   attr(populacao$fxetaria, which = "label") <- "Faixa etaria detalhada"
+   attr(populacao$munic_res, which = "label") <- "Codigo IBGE do municipio"
+   # Hmisc::label(populacao$fxetar5) <- "Faixa etaria quinquenal"
+   # names(populacao)[5] <- "fxetar.det"
+   # Hmisc::label(populacao$fxetar.det) <- "Faixa etaria detalhada"
+   # Hmisc::label(populacao$fxetaria) <- "Faixa etaria detalhada"
+   # Hmisc::label(populacao$munic_res) <- "Codigo IBGE do municipio"
+   return(populacao)
   }
