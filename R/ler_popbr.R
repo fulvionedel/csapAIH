6652f7b309371d536dcf07cb5fc224113b70d9f2
Fúlvio B. Nedel <fulvionedel@gmail.com>
2023-04-15 18:38:18 -0300

    Reestruturada

--- R/ler_popbr.R
+++ R/ler_popbr.R
@@ -1,50 +1,64 @@
-#' @title Ler arquivos POPBR????.CSV/DBF
+#' @title Ler arquivos POPBR????.DBF
 #' @aliases ler_popbr
 #' @description Lê os arquivos de população do DATASUS e cria uma variável com a faixa etária quinquenal
 #'
-#' @param pop População. Nome do arquivo a ser lido.
-#' @param source Fonte de dados. Use `file` (padrão) para um arquivo no computador ou `url` para baixar o arquivo do FTP do DATASUS.
+#' @param arquivo Nome do arquivo DBF armazenado no computador. Padrão é \code{NULL}, v. details.
+#' @param ano Ano da estimativa ou contagem populacional a ser capturada no site FTP DATASUS. Padrão é \code{NULL}, v. details.
+#'
+#' @details Apenas um dos dois parâmetros deve ser preenchido. Se a
 #'
 #' @examples
 #' \dontrun{
 #' # Um arquivo no mesmo diretório de trabalho da sessão ativa:
-#' popBR2012 <- ler_popbr("POPBR12.DBF")
-#' head(popBR2012)
-#' xtabs(populacao ~ fxetar5, data = popBR2012)
+#' popBR2010 <- ler_popbr("data-raw/POPBR10.DBF")
+#' head(popBR2010)
+#' xtabs(populacao ~ fxetar5 + sexo, data = popBR2010)
 #' }
 #'
 #' # Um arquivo no diretório FTP do DATASUS
-#' popBR2012 <- ler_popbr("popBR12", source = "url")
-#' xtabs(populacao ~ fxetar5, data = popBR2012)
+#' popBR2010 <- ler_popbr(ano = 2010)
+#' xtabs(populacao ~ fxetar5 + sexo, data = popBR2010)
 #'
 #' @importFrom utils download.file unzip
 #' @importFrom Hmisc upData
 #' @export
 
-ler_popbr <- function (pop, source = "file")
-  {
-   if(source == "url"){
-      pop <- toupper(pop)
-      url <- paste0("ftp://ftp.datasus.gov.br/dissemin/publicos/IBGE/POP/", pop, ".zip")
-      temp <- tempfile()
-      download.file(url, temp)
-      unzip(temp, paste0(pop, ".DBF"))
-      populacao <- foreign::read.dbf(paste0(pop, ".DBF"))
-      unlink(temp)
-      unlink(paste0(pop, ".DBF"))
-    } else if(source == "file") populacao <- foreign::read.dbf(pop)
+ler_popbr <- function (arquivo = NULL, ano = NULL) {
 
-   populacao <- Hmisc::upData(populacao, lowernames = T, print = F)
-   populacao$fxetar5 <- cut(as.numeric(populacao$fxetaria),
-                            breaks = c(0, 5, 10, 15, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33))
-   levels(populacao$fxetar5) <- c("0-4", "5-9", "10-14", "15-19",
-                                  "20-24", "25-29", "30-34", "35-39", "40-44", "45-49",
-                                  "50-54", "55-59", "60-64", "65-69", "70-74", "75-79",
-                                  "80 +")
+  if(!is.null(ano)) {
+    if(ano %in% 1980:2012 == FALSE) {
+      stop("S\u00f3 h\u00e1 arquivos dispon\u00edveis para os anos 1980 a 2012.")
+    }
+    pop <- paste0("POPBR", substr(as.character(ano), 3,4))
+    url <- paste0("ftp://ftp.datasus.gov.br/dissemin/publicos/IBGE/POP/", pop, ".zip")
+    temp <- tempfile()
+    download.file(url, temp)
+    unzip(temp, paste0(pop, ".DBF"))
+    populacao <- foreign::read.dbf(paste0(pop, ".DBF"))
+    unlink(temp)
+    unlink(paste0(pop, ".DBF"))
+  } else if(!is.null(arquivo)) populacao <- foreign::read.dbf(arquivo)
+
+  populacao <- Hmisc::upData(populacao, lowernames = T, print = F)
+
+  if(unique(populacao$ano) %in% c(1980, 1991, 1996:2012)) {
+    populacao$fxetar5 <- cut(as.numeric(populacao$fxetaria),
+                             breaks = c(0, 5, 10, 15, 20:33))
+  } else if(unique(populacao$ano) %in% c(1981:1990, 1992)) {
+    populacao$fxetar5 <- populacao$fxetaria
+  } else if(unique(populacao$ano) %in% 1993:1995) {
+     populacao$fxetar5 <- cut(as.numeric(populacao$fxetaria),
+                              breaks = c(0, 5:21))
+   }
+  levels(populacao$fxetar5) <- c(  "0-4",   "5-9", "10-14", "15-19", "20-24",
+                                 "25-29", "30-34", "35-39", "40-44", "45-49",
+                                 "50-54", "55-59", "60-64", "65-69", "70-74",
+                                 "75-79", "80 +")
+
    # pop$fxetaria <- factor(pop$fxetaria, levels = 1:33, labels = c("< 1 ano", 1:19, levels(pop$fxetar5)[5:17]))
    # levels(populacao$fxetaria) <- c("< 1 ano", 1:19, levels(populacao$fxetaria)[20:33]) #levels(populacao$fxetar5)[5:17])
    levels(populacao$sexo) <- c("masc", "fem")
-   if ( length( levels(populacao$situacao)>1 ) ) {
+   if ( length( levels(populacao$situacao) > 1 ) ) {
       levels(populacao$situacao) <- c("urbana", "rural")
     }
    # populacao$ano <- as.integer(as.character(populacao$ano))
