e7b74989f6c69aaf34203d84ef58e4c635a9034f
Fúlvio B. Nedel <fulvionedel@gmail.com>
2023-04-16 21:53:58 -0300

    Melhora ajuda

--- R/fxetar_quinq.R
+++ R/fxetar_quinq.R
@@ -1,7 +1,7 @@
 #' @title Categoriza a idade em faixas etárias quinquenais
 #' @aliases fxetar_quinq
 #'
-#' @description Categoriza um vetor de valores contínuos ou inteiros em tantas faixas quinquenais. A primeira faixa pode ser quebrada em `< 1 ano` e `1-4`, com o argumento `puer = TRUE`. Serão tantas faixas quantas definidas pelos argumentos `puer` e `senectus`. Por padrão (`senectus = 80`) são 17 faixas quinquenais: 0-4, ..., 80 e +.
+#' @description Categoriza um vetor de valores contínuos ou inteiros em faixas quinquenais. A primeira faixa pode ser quebrada em `< 1 ano` e `1-4`, com o argumento `puer = TRUE`. O número de faixas etárias é definido pelos argumentos `puer` e `senectus`. Por padrão (`puer = FALSE` e `senectus = 80`) são 17 faixas quinquenais: 0-4, ..., 80 e +.
 #
 #' @param aetas Se `NULL` (padrão), retorna um vetor com as faixas etárias definidas por `puer` e `senectus`. Se um vetor com valores numéricos (`dbl`, `num`, `int`), idealmente uma variável com valores de idade, classifica o valor na faixa etária.
 #' @param senectus Um valor definindo o início do último intervalo, que é aberto. Com o padrão `senectus = 80`, a função retorna um fator (`fct`) com 17 nívels (`levels`) em que o último é "80 e +"
