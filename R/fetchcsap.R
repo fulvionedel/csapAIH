65dd9f0dcf9409c703b90fad0c907eafbdf886d6
Fúlvio B. Nedel <fulvionedel@gmail.com>
2024-09-22 15:21:39 -0300

    Novos parâmetros, para extração de dados por ano e mês de competência do arquivo RD ou por data de internação.

--- R/fetchcsap.R
+++ R/fetchcsap.R
@@ -4,12 +4,16 @@
 #' @description
 #' Descarrega os "arquivos da AIH" (arquivos RD<UFAAMM>.DBC das Bases de Dados do Sistema de Informações Hospitalares do SUS - BD-SIH/SUS) do site FTP do DATASUS e classifica as internações segundo a Lista Brasileira de Condições Sensíveis à Atenção Primária.
 #'
-#' @param uf        Unidade da Federação. A sigla da UF ou um vetor com as siglas das UF de interesse, entre aspas e em letras maiúsculas. Para todo o Brasil (padrão), use "all".
-#' @param mesinicio Mês de competência da AIH para início da seleção dos dados, em formato numérico; por padrão é 1
-#' @param anoinicio Ano de competência da AIH para início da seleção dos dados, em formato numérico; sem padrão
-#' @param mesfim    Mês de competência da AIH para fim da seleção dos dados, em formato numérico; por padrão é igual ao mês de início
-#' @param anofim    Ano de competência da AIH para fim da seleção dos dados, em formato numérico; por padrão é igual ao ano de início
-#' @param ...       Permite o uso de parâmetros de \code{\link{csapAIH}}
+#' @param uf Unidade da Federação. A sigla da UF ou um vetor com as siglas das UF de interesse, entre aspas e em letras maiúsculas. Para todo o Brasil (padrão), use "all".
+#' @param mesinicio Mês de competência da AIH para início da seleção dos dados, em formato numérico; por padrão é 1.
+#' @param anoinicio Ano de competência da AIH para início da seleção dos dados, em formato numérico; sem padrão.
+#' @param mesfim Mês de competência da AIH para fim da seleção dos dados, em formato numérico; por padrão é 6 (junho). V. detalhes.
+#' @param anofim Ano de competência da AIH para fim da seleção dos dados, em formato numérico; por padrão é igual ao ano seguinte ao ano de início (\code{anoinicio + 1}).
+#' @param periodo O período definido refere-se ao mês e ano de "competência" da AIH ou à data de internação? O padrão (\code{"interna"}) é a internação. V. detahes.
+#' @param fim.periodo Se o período for definido pela data de internação, qual a data final? O padrão é 31 de dezembro do ano anterior ao definido no argumênto \code{anofim}. V. detalhes.
+#' @param  cep CEP de internação
+#' @param cnes CNES do estabelecimento que gerou a AIH
+#' @param ... Permite o uso de outros parâmetros de \code{\link{csapAIH}}
 #'
 #' @returns Um objeto de classes \code{data.table} e \code{data.frame} com as seguintes variáveis:
 #'  \itemize{
@@ -17,7 +21,6 @@
 #'   \item \code{munint} Município de internação do paciente
 #'   \item \code{sexo} Sexo do paciente
 #'   \item \code{idade} Idade do paciente em anos completos
-#'   \item \code{fxetar.det} Faixa etária detalhada
 #'   \item \code{fxetar5} Faixa etária quinquenal (0-4, ..., 76-79, 80 e +)
 #'   \item \code{csap} Internação por CSAP (sim/não)
 #'   \item \code{grupo} Grupo de causa da Lista Brasileira de ICSAP, ou "não-CSAP"
@@ -27,31 +30,48 @@
 #'  }
 #'
 #' @details
+#'
+#' Período de download dos arquivos e de internação dos sujeitos.
+#' \itemize{
+#'   \item Os "arquivos da AIH" são definidos por mês e ano de "competência", e não da data de internação. Assim, o arquivo de um determinado "mês de competência" pode incluir registros de internações ocorridas em outro mês ou ano, enquanto pode não incluir todos os casos ocorridos naquele mês. Por padrão, \code{fetchcsap} usa o argumento \code{periodo = "interna"} para selecionar os casos por data de internação de acordo com o período definido nos argumentos \code{anoinicio}, \code{mesinicio} e \code{anofim}, de modo a iniciar no primeiro dia do ano e mês de competência (\code{anoinicio} e \code{mesinicio}) e terminar em 31 de dezembro do ano anterior ao definido em \code{anofim}.
+#'   \item Assim, por padrão, a função exige apenas a definição do ano de início dos casos. Se o usuário definir apenas esse argumento, \code{fetchcsap} fará o download e leitura dos arquivos (de todo o Brasil) de todos os meses até junho do ano seguinte para então selecionar as internações ocorridas no ano definido em \code{anoinicio}.
+#' }
+#'
 #' \code{fetchcsap} é apenas uma abreviatura para um uso específico da função \code{fetch_datasus}, do pacote \code{microdatasus}, de Raphael Saldanha. Funciona apenas com o SIH/SUS, através do argumento \code{information_system = "SIH-RD"}, e faz apenas o download das variáveis exigidas pela função \code{csapAIH}, i.e., \code{DIAG_PRINC, NASC, DT_INTER, DT_SAIDA, IDADE, COD_IDADE, MUNIC_RES, MUNIC_MOV, SEXO, N_AIH, PROC_REA, IDENT, CEP, CNES}.
 #'
 #' @seealso \code{\link{csapAIH}}
 #'
 #' @examples
-#' # Internações de todo o Brasil, "mês de competência" janeiro de 2023
+#' # Internações de todo o Brasil, ocorridas no ano de 2023 e registradas até jun/2024:
+#' # Colocando apenas o ano, único argumento obrigatório, a função executa o
+#' # download dos arquivos RD??????.DBC de todas as UF de jan/2023 a jun/2024 e
+#' # então extrais apenas os registros com data de internação em 2023.
 #' \dontrun{
 #'   fetchcsap(anoinicio = 2023)
 #' }
-#' # Internações no RS, mês de competência jan 2023:
-#' fetchcsap("RS", 2023)
-#' ## Internações no RS, ano de competência 2023:
-#' \dontrun{
-#' fetchcsap("RS", 2023, mesfim = 12) }
+#' # Diferença entre o mês e ano de "competência" da AIH e a data de internação da pessoa,
+#' # exemplo com as internações no Acre, registradas no mês de competência jan 2023:
+#' ac.comp <- fetchcsap("AC", 2023, mesfim = 1, periodo = 'competencia')
+#' nrow(ac.comp)
+#' summary(ac.comp$data.inter)
+#' ac.int <- fetchcsap("AC", 2023, mesfim = 1)
+#' nrow(ac.int)
+#' summary(ac.int$data.inter)
+#' # Assim, há
+#' nrow(ac.comp) - nrow(ac.int)
+#' # internações registradas naquele mês de competência, mas que ocorreram em outro momento.
 #'
 #' @importFrom data.table setDT `:=`
 #'
 #' @export
-fetchcsap <- function(uf = "all", anoinicio, mesinicio = 1, mesfim = mesinicio, anofim = anoinicio, ...) {
+fetchcsap <- function(uf = "all", anoinicio, mesinicio = 1,
+                      anofim = anoinicio + 1 , mesfim = 6,
+                      periodo = "interna", fim.periodo = NULL,
+                      cep = FALSE, cnes = FALSE, ...) {
 ':=' <- setDT <- DT_INTER <- idade <- NULL
 
   # Seleção de variáveis da AIH
   vars <- c("DIAG_PRINC", "NASC", "DT_INTER", "DT_SAIDA", "IDADE", "COD_IDADE", "MUNIC_RES", "MUNIC_MOV", "SEXO", "N_AIH", "PROC_REA", "IDENT", "CEP", "CNES")
-
-  # if(is.null(anofim)) { anofim <- anoinicio }
 
   # Baixar os dados do DATASUS
   aih  <- microdatasus::fetch_datasus(year_start  = anoinicio,
@@ -64,16 +84,23 @@ fetchcsap <- function(uf = "all", anoinicio, mesinicio = 1, mesfim = mesinicio, 
                                       vars = vars)
 
   # Selecionar o período de interesse
-  # setDT(aih)
-  # aih <- aih[as.character(DT_INTER) >= "20170101", ]
-  # aih <- aih[as.character(DT_INTER) <= "20191231", ]
-  # aih <- data.frame(aih)
-
+  if( periodo == "interna") {
+    mesinicio <- ifelse(mesinicio < 10, paste0("0", mesinicio), mesinicio)
+    peri <- paste0(anoinicio, mesinicio, "01")
+    if( is.null(fim.periodo) ) {
+      perf <- paste0(anofim - 1, "1231")
+    }
+    setDT(aih)
+    aih <- aih[as.character(DT_INTER) >= peri, ]
+    aih <- aih[as.character(DT_INTER) <= perf, ]
+    aih <- data.frame(aih)
+  }
+#
   # Classificar as CSAP
-  # csap <- setDT(
-  csap <- csapAIH::csapAIH(aih, ...)
-  # )
+  csap <- csapAIH::csapAIH(aih, cep = cep, cnes = cnes, ...) |>
+    setDT()
+  csap <- csap[, c("fxetar.det", "nasc", "n.aih", "proc.rea") := NULL]
 
   # O banco final (produto da função)
-  csap
+  csap[]
 }
