03a98b364dc78195795aab9a8cc35b2cc7ae7705
Fúlvio B. Nedel <fulvionedel@gmail.com>
2025-02-15 13:15:32 -0300

    Corrige erros anteriores e inclui os arquivos atualizados com o censo

--- R/ler_popbr.R
+++ R/ler_popbr.R
@@ -1,52 +1,85 @@
 #' @title Ler arquivos de população do DATASUS
 #' @aliases ler_popbr
-#' @description Lê os arquivos POPBR?? com estimativas e contagens da população dos municípios brasileiros por sexo e faixa etária disponibilizados pelo DATASUS e cria uma variável com a faixa etária quinquenal.
+#' @description Lê os arquivos com estimativas e contagens da população dos municípios brasileiros por sexo e faixa etária disponibilizados pelo DATASUS e entrega um banco de dados com as variáveis originais mais a faixa etária quinquenal.
 #'
-#' @param arquivo Nome do arquivo armazenado no computador, em formato CSV ou DBF. Padrão é \code{NULL}, v. details.
-#' @param ano Ano da estimativa ou contagem populacional a ser capturada no site FTP DATASUS. Padrão é \code{NULL}, v. details.
-#'
-#' @details Apenas um dos dois parâmetros deve ser preenchido. Se o alvo é um arquivo no computador, o nome com a extensão (csv ou dbf) deve vir entre aspas; se for um arquivo csv, deve ser separado por vírgulas (é usada a função \link{read.csv} e não \link{read.csv2}). Se o alvo é um arquivo do servidor FTP do datasus, deve-se usar o argumando \code{ano}, com o ano (sem aspas) desejado, de 1980 a 2012.
+#' @param x Nome do arquivo armazenado no computador, ou ano da estimativa ou contagem populacional a ser capturada no site FTP DATASUS. Se o alvo é um arquivo no computador, o nome com a extensão (dbf) deve vir entre aspas. Se o alvo é um arquivo do servidor FTP do DATASUS, deve-se usar o argumento \code{ano}, com o ano (sem aspas) desejado, de 1980 a 2024. Apenas arquivos em formato DBF são lidos.
 #'
 #' @examples
 #' \dontrun{
-#' # Um arquivo no mesmo diretório de trabalho da sessão ativa:
+#' # Um arquivo no computador:
 #' popBR2010 <- ler_popbr("data-raw/POPBR10.DBF")
 #' head(popBR2010)
 #' xtabs(populacao ~ fxetar5 + sexo, data = popBR2010)
+#'
+#' popBR2024 <- ler_popbr("data-raw/POPBR24.DBF")
+#' head(popBR2024)
+#' xtabs(populacao ~ fxetar5 + sexo, data = popBR2024)
 #' }
 #'
 #' # Um arquivo no diretório FTP do DATASUS
-#' popBR2010 <- ler_popbr(ano = 2010)
+#' popBR2010 <- ler_popbr(2010)
+#' head(popBR2010)
 #' xtabs(populacao ~ fxetar5 + sexo, data = popBR2010)
 #'
-#' @importFrom utils download.file unzip read.csv
+#' popBR2013 <- ler_popbr(2013)
+#' head(popBR2013)
+#' xtabs(populacao ~ fxetar5 + sexo, data = popBR2013)
+#'
+#' popBR2024 <- ler_popbr(2024)
+#' head(popBR2024)
+#' xtabs(populacao ~ fxetar5 + sexo, data = popBR2024)
+#'
+#' @importFrom utils download.file unzip
 #' @export
 
-ler_popbr <- function (arquivo = NULL, ano = NULL) {
+ler_popbr <- function(x) {
+
+  ano <- arquivo <- NULL
+  if(x %in% 1980:2024) ano = x
+    else arquivo = x
 
   if(!is.null(ano)) {
-    if(ano %in% 1980:2012 == FALSE) {
-      stop("S\u00f3 h\u00e1 arquivos dispon\u00edveis para os anos 1980 a 2012.")
+    if(ano %in% 1980:2012) {
+      pop <- paste0("POPBR", substr(as.character(ano), 3,4))
+      url <- paste0("ftp://ftp.datasus.gov.br/dissemin/publicos/IBGE/POP/", pop, ".zip")
+      temp <- tempfile()
+      download.file(url, temp)
+      unzip(temp, paste0(pop, ".DBF"))
+      populacao <- foreign::read.dbf(paste0(pop, ".DBF"))
+      # populacao$SITUACAO <- NULL
+      unlink(temp)
+      unlink(paste0(pop, ".DBF"))
+    } else if(ano %in% 2013:2024){
+      pop <- paste0("POPSBR", substr(as.character(ano), 3,4))
+      url <- paste0("ftp://ftp.datasus.gov.br/dissemin/publicos/IBGE/POPSVS/", pop, ".zip")
+      pop <- paste0(substr(pop, 1,3), substr(pop, 7,8))
+      if(ano %in% 2013:2021) {
+        pop <- tolower(pop)
+      }
+      temp <- tempfile()
+      download.file(url, temp)
+      unzip(temp, paste0(pop, ".dbf"))
+      populacao <- foreign::read.dbf(paste0(pop, ".dbf"))
+      unlink(temp)
+      unlink(paste0(pop, ".dbf"))
+    } else if(ano %in% 1980:2024 == FALSE){
+      stop("S\u00f3 h\u00e1 arquivos dispon\u00edveis para os anos 1980 a 2024.")
     }
-    pop <- paste0("POPBR", substr(as.character(ano), 3,4))
-    url <- paste0("ftp://ftp.datasus.gov.br/dissemin/publicos/IBGE/POP/", pop, ".zip")
-    temp <- tempfile()
-    download.file(url, temp)
-    unzip(temp, paste0(pop, ".csv"))
-    populacao <- read.csv(paste0(pop, ".csv"))
-    unlink(temp)
-    unlink(paste0(pop, ".csv"))
   } else if(!is.null(arquivo)) {
-    if(grepl('dbf', ignore.case = TRUE) %in% arquivo) {
+    if(grepl('dbf', arquivo, ignore.case = TRUE)) {
       populacao <- foreign::read.dbf(arquivo)
-    } else if(grepl('csv', ignore.case = TRUE) %in% arquivo) {
-      populacao <- read.csv(arquivo)
     }
   }
 
   names(populacao) <- tolower(names(populacao))
 
-  if(unique(populacao$ano) %in% c(1980, 1991, 1996:2012)) {
+  if( unique(populacao$ano) %in% 2013:2024) {
+    names(populacao)[1] <- "munic_res"
+    names(populacao)[4] <- "fxetaria"
+    names(populacao)[5] <- "populacao"
+  }
+
+  if(unique(populacao$ano) %in% c(1980, 1991, 1996:2024)) {
     populacao$fxetar5 <- cut(as.numeric(populacao$fxetaria),
                              breaks = c(0, 5, 10, 15, 20:33))
   } else if(unique(populacao$ano) %in% c(1981:1990, 1992)) {
@@ -54,7 +87,8 @@ ler_popbr <- function (arquivo = NULL, ano = NULL) {
   } else if(unique(populacao$ano) %in% 1993:1995) {
      populacao$fxetar5 <- cut(as.numeric(populacao$fxetaria),
                               breaks = c(0, 5:21))
-   }
+  }
+
   levels(populacao$fxetar5) <- c(  "0-4",   "5-9", "10-14", "15-19", "20-24",
                                  "25-29", "30-34", "35-39", "40-44", "45-49",
                                  "50-54", "55-59", "60-64", "65-69", "70-74",
