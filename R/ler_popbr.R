17437b26dcf4f340b29e3fea6446db331fd715a2
Fúlvio B. Nedel <fulvionedel@gmail.com>
2025-08-31 22:53:51 -0300

    Computa idade pela função 'fxetar_quinq()', para padronizar rótulo de "80 e +"

--- R/ler_popbr.R
+++ R/ler_popbr.R
@@ -114,13 +114,11 @@ ler_popbr <- function(x) {
   populacao$munic_res <- as.character(populacao$munic_res) |> substr(1,6)
 
   # Rótulos para a faixa etária
-  levels(populacao$fxetar5) <- c(  "0-4",   "5-9", "10-14", "15-19", "20-24",
-                                 "25-29", "30-34", "35-39", "40-44", "45-49",
-                                 "50-54", "55-59", "60-64", "65-69", "70-74",
-                                 "75-79", "80 +")
-
-   # pop$fxetaria <- factor(pop$fxetaria, levels = 1:33, labels = c("< 1 ano", 1:19, levels(pop$fxetar5)[5:17]))
-   # levels(populacao$fxetaria) <- c("< 1 ano", 1:19, levels(populacao$fxetaria)[20:33]) #levels(populacao$fxetar5)[5:17])
+  levels(populacao$fxetar5) <- fxetar_quinq() # Padronizar rótulos ("80 e +")
+  # levels(populacao$fxetar5) <- c(  "0-4",   "5-9", "10-14", "15-19", "20-24",
+  #                                "25-29", "30-34", "35-39", "40-44", "45-49",
+  #                                "50-54", "55-59", "60-64", "65-69", "70-74",
+  #                                "75-79", "80 +")
    levels(populacao$sexo) <- c("masc", "fem")
    if ( length( levels(populacao$situacao) > 1 ) ) {
       levels(populacao$situacao) <- c("urbana", "rural")
